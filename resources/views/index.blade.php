ec68c075123ad038726c766e6a30b6a45f9cc7eb
FebiRamdanii <60900119012@uin-alauddin.ac.id>
2021-11-22 22:02:52 +0800

    commit pertama branch header

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>SelamatDatang</title>
+</head>
+<body>
+    <h1>Hayy,Selamat Datang Dilaporan 1,</h1> 
+        <a href="{{url(route('BiodataDiri'))}}">Silahkan Ke Biodata Diri </a>
+</body>
+</html>
